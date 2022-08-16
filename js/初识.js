b9cac5150c1148ee783a2334c271c49889b19958
wjd <196386373@qq.com>
2022-08-16 17:18:47 +0800

    学习js

---
+++ js/初识.js
@@ -0,0 +1,17 @@
+function num (a,b,f){
+    switch(f){
+        case '+':
+            return(a + b);
+            break;
+        case '-':
+            return(a - b);
+            break;
+        case '*':
+            return (a * b);
+            break;
+        case '/':
+            
+            return (a / b);
+    }
+    // return res;
+}
